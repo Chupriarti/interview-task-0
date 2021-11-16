c48812dfd783f9194003715676bfbd842b4817b3
Chupriarti <wallx13@gmail.com>
2021-11-16 21:16:22 +0300

    added store

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { store } from './store';
 
 ReactDOM.render(
-    <Provider store={}>
+    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
