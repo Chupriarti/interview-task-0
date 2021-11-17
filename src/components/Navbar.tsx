af2be2caa688e437ca583e01b85ffe4fc39fd046
Chupriarti <wallx13@gmail.com>
2021-11-17 21:12:48 +0300

    added menu utem callback

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,19 @@
 import {Layout, Menu, Row} from 'antd';
 import React, {FC} from 'react'
+import {useHistory} from 'react-router-dom';
+import { RouteName } from '../routes';
 
 const Navbar: FC = () => {
+  const router = useHistory();
   return (
     <Layout.Header >
       <Row justify="end">
         <Menu  theme="dark" mode="horizontal" selectable={false}>
-          <Menu.Item key={1}>Логин</Menu.Item>
+          <Menu.Item 
+            onClick={() => router.push(RouteName.LOGIN)}
+            key={1}>
+              Логин
+            </Menu.Item>
         </Menu>
       </Row>
     </Layout.Header>
