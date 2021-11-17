822fbba1665789f376a72498bafcf79b8580426d
Chupriarti <wallx13@gmail.com>
2021-11-17 21:30:38 +0300

    prepared auth reducer

---
+++ src/store/reducers/auth/index.ts
@@ -0,0 +1,15 @@
+import { AuthAction, AuthActionEnum, AuthState } from "./types";
+
+const initialState: AuthState = {
+    auth: false
+}
+
+export default function authReducers(state = initialState, action: AuthAction): AuthState{
+    switch (action.type){
+        case AuthActionEnum.SET_AUTH:
+            return {...state, auth: action.payload}
+        default:
+            return state;
+    }
+
+}
