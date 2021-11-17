7c3401f2f19eb92fff418e4b137d4ecebd579497
Chupriarti <wallx13@gmail.com>
2021-11-17 21:10:02 +0300

    added menu item

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
-import {Layout, Row} from 'antd';
+import {Layout, Menu, Row} from 'antd';
 import React, {FC} from 'react'
 
 const Navbar: FC = () => {
   return (
-    <Layout.Header>
+    <Layout.Header >
       <Row justify="end">
-      Navbar
+        <Menu  theme="dark" mode="horizontal" selectable={false}>
+          <Menu.Item key={1}>Логин</Menu.Item>
+        </Menu>
       </Row>
     </Layout.Header>
   )
