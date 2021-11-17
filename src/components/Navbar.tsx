c24abd1ec790e941b3da5c2e046718cdb67c8d44
Chupriarti <wallx13@gmail.com>
2021-11-17 21:16:19 +0300

    added menu item exit based on auth

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,16 +5,29 @@ import { RouteName } from '../routes';
 
 const Navbar: FC = () => {
   const router = useHistory();
+  const auth = false;
   return (
     <Layout.Header >
       <Row justify="end">
-        <Menu  theme="dark" mode="horizontal" selectable={false}>
-          <Menu.Item 
-            onClick={() => router.push(RouteName.LOGIN)}
-            key={1}>
-              Логин
-            </Menu.Item>
-        </Menu>
+        {auth
+          ?
+            <Menu  theme="dark" mode="horizontal" selectable={false}>
+              <Menu.Item 
+                onClick={() => console.log("Exiting...")}
+                key={1}>
+                  Выйти
+                </Menu.Item>
+            </Menu>
+          :
+            <Menu  theme="dark" mode="horizontal" selectable={false}>
+              <Menu.Item 
+                onClick={() => router.push(RouteName.LOGIN)}
+                key={1}>
+                  Логин
+                </Menu.Item>
+            </Menu>
+        }
+ 
       </Row>
     </Layout.Header>
   )
