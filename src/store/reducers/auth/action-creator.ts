c8a47719f82085d6d043512e3e8f0adbfae53b5b
Chupriarti <wallx13@gmail.com>
2021-11-18 22:08:29 +0300

    prepared login-logout actrion creators

--- src/store/reducers/auth/action-creator.ts
+++ src/store/reducers/auth/action-creator.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+import { AppDispatch } from "../..";
 import { IUser } from "../../../models/IUser";
 import { AuthActionEnum, SetAuthAction, SetErrorAction, SetLoadingAction, SetUserAction } from "./types";
 
@@ -5,5 +7,21 @@ export const AuthActionCreators = {
     setUser: (user: IUser): SetUserAction => ({type: AuthActionEnum.SET_USER, payload: user}),
     setIsAuth: (isAuth: boolean): SetAuthAction => ({type: AuthActionEnum.SET_AUTH, payload: isAuth}),
     setIsLoading: (isLoading: boolean): SetLoadingAction => ({type: AuthActionEnum.SET_IS_LOADING, payload: isLoading}),
-    setError: (error: string): SetErrorAction => ({type: AuthActionEnum.SET_ERROR, payload: error})
+    setError: (error: string): SetErrorAction => ({type: AuthActionEnum.SET_ERROR, payload: error}),
+    login: (username: string, password: string) => async (dispatch: AppDispatch) => {
+        try {
+            dispatch(AuthActionCreators.setIsLoading(true));
+            const mockUser = await axios.get('./users.json');
+            console.log(mockUser)
+        } catch (e) {
+            dispatch(AuthActionCreators.setError('Произошла ошибка при логине'));
+        }
+    },
+    logout: () => async (dispatch: AppDispatch) => {
+        try {
+            
+        } catch (e) {
+
+        }
+    },
 }
