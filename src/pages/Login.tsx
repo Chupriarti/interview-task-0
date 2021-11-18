dab65aa51fb550db6ad0f75726c435933c4d0d30
Chupriarti <wallx13@gmail.com>
2021-11-18 21:34:10 +0300

    added LoginForm to Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,12 @@
 import { Layout, Row } from 'antd';
 import React, {FC} from 'react'
+import LoginForm from '../components/LoginForm';
 
 const Login: FC = () => {
   return (
     <Layout>
       <Row justify="center" align="middle" className="h100">
-        LOGIN 
+        <LoginForm /> 
       </Row>
     </Layout>
   )
