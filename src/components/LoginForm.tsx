ff7dc6845c02ddd7dc73b45b2cc48679de06d211
Chupriarti <wallx13@gmail.com>
2021-11-18 21:43:49 +0300

    prepared submit callback

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -3,8 +3,14 @@ import React, {FC} from 'react'
 import { rules } from '../utils/rules';
 
 const LoginForm: FC = () => {
+  const submit = () => {
+      console.log("submited!")
+  }
+
   return (
-    <Form>
+    <Form
+      onFinish={submit}
+    >
       <Form.Item
         label="Имя пользователя"
         name="username"
