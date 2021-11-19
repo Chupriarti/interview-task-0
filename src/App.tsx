21e43713076a9f742cdbf12a968632b3b5344e59
Chupriarti <wallx13@gmail.com>
2021-11-19 22:20:39 +0300

    added remember login logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,18 @@ import React, {FC} from 'react'
 import AppRouter from './components/AppRouter';
 import Navbar from './components/Navbar';
 import './App.css'
+import { useActions } from './hooks/useActions';
+import { IUser } from './models/IUser';
 
 const App: FC = () => {
+  const {setUser, setIsAuth} = useActions();
+  React.useEffect(() => {
+    if (localStorage.getItem('auth')){
+      setUser({username: localStorage.getItem('username' || '')} as IUser);
+      setIsAuth(true);
+    }
+  }, [])
+
   return (
     <Layout>
       <Navbar />
