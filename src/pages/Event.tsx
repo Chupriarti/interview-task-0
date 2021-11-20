bf66533020e3416c0882187a3b8d80b3712d33bd
Chupriarti <wallx13@gmail.com>
2021-11-20 18:11:02 +0300

    prepared Modal windows for adding event

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,11 +1,25 @@
-import { Layout } from 'antd';
+import { Button, Layout, Modal, Row } from 'antd';
 import React, {FC} from 'react'
 import EventCalendar from '../components/EventCalendar';
 
 const Event: FC = () => {
+  const [modalVisible, setModalVisible] = React.useState(false);
+  console.log(modalVisible)
   return (
     <Layout>
       <EventCalendar events={[]} />
+      <Row justify="center">
+        <Button
+          onClick={() => setModalVisible(true)}
+        >
+          Добавить событие
+        </Button>
+      </Row>
+      <Modal
+        title="Добавить событие"
+        visible={modalVisible}
+      >
+      </Modal>
     </Layout>
   )
 }
